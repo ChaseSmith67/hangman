c242b4aa205e0f31004dd360c66586d1817c8c11
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-08-18 16:14:37 -0500

    add initial word list

--- hangman.c
+++ hangman.c
@@ -4,6 +4,59 @@
 
 int main() {
 
+    char wordList[][20] = {
+        "alabaster"
+        "apple",
+        "ballerina",
+        "basketball",
+        "concertina",
+        "copper",
+        "didgeridoo",
+        "doctor",
+        "eggplant",
+        "elephant",
+        "fountain",
+        "furniture",
+        "gander",
+        "giggle",
+        "hamburger",
+        "holiday",
+        "icecicle",
+        "insurance",
+        "jackal",
+        "jelly",
+        "kangaroo",
+        "kite",
+        "library",
+        "lunch",
+        "machine",
+        "motorcycle",
+        "noise",
+        "notebook",
+        "oyster",
+        "operation",
+        "penguin",
+        "pineapple",
+        "quilt",
+        "queen",
+        "racecar",
+        "robot",
+        "salamander",
+        "saxophone",
+        "tablecloth",
+        "trombone",
+        "umbrella",
+        "underwear",
+        "vanguard",
+        "vibration",
+        "walrus",
+        "whimsy",
+        "xylophone",
+        "yacht",
+        "zebra"
+    };
+
+    
 
     return 0;
 }
